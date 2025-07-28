9bad1523675229f4ab9bdb8acf2c539ca70bf401
melisacar <acarme17@itu.edu.tr>
2025-07-28 14:55:02 +0300

    feat: add kafka weather data producer with api integration

---
+++ src/producer.py
@@ -0,0 +1,26 @@
+from kafka import KafkaProducer
+import json
+import requests
+import time
+
+producer = KafkaProducer(
+    bootstrap_servers=('KAFKA_BROKERS', 'kafka:9092'), # Container DNS
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    )
+
+topic = 'weather-data'
+
+def fetch_weather_data():
+    url = "https://api.weather.gov/gridpoints/LOX/150,48/forecast"
+    headers = {"User-Agent": "myweatherapp.com"}
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    return data["properties"]["periods"]
+
+while True:
+    weather_periods = fetch_weather_data()
+    for item in weather_periods:
+        producer.send(topic, item)
+        print(f"Sent: {item}")
+        time.sleep(5)
+    time.sleep(300)
